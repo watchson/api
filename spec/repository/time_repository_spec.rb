17d220c5bd15a1f95b82ac838783bd62da63aed8
Dawson Israel <dawfre@amazon.com>
2019-07-03 10:37:55 -0300

    Add TimeRepository tests

--- spec/repository/time_repository_spec.rb
+++ spec/repository/time_repository_spec.rb
@@ -1,14 +1,16 @@
 require_relative '../../app/repository/time_repository'
 
-RSpec.describe TimeRepository, "TimeRepository" do
+describe TimeRepository, "TimeRepository" do
   context "Add Time" do
+    before(:each) do
+      @dynamo_db = double(:put_item => nil)
+      @time_repository = TimeRepository.send :new, @dynamo_db
+    end
+
     it "with all parameters" do
-      dynamo_db = double(:put_item => nil)
-      time_repository = TimeRepository.send :new, dynamo_db
+      @time_repository.add_time("potato", "2019-01-01 01:02:03", false, true, "Batata")
 
-      time_repository.add_time("potato", "2019-01-01 01:02:03", false, true, "Batata")
-
-      expect(dynamo_db).to have_received(:put_item) do |arg|
+      expect(@dynamo_db).to have_received(:put_item) do |arg|
         expect(arg[:table_name]).to eq("Time")
         expect(arg[:item][:user_id]).to eq("potato")
         expect(arg[:item][:registered_date]).to eq("2019-01-01 01:02:03")
@@ -18,6 +20,12 @@ RSpec.describe TimeRepository, "TimeRepository" do
         expect(arg[:item][:created_at]).to be_truthy
         expect(arg[:item][:updated_at]).to be_truthy
       end
+    end
+
+    it "when DynamoDB throws error" do
+      allow(@dynamo_db).to receive(:put_item).and_raise(ArgumentError.new)
+
+      expect{@time_repository.add_time("potato", "2019-01-01 01:02:03", false, true, "Batata")}.to raise_error(ArgumentError)
     end
   end
 end
