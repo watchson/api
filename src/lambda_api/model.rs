c513992775ce0e29153958bb735a61838b058c1e
Dawson Israel <dawfre@amazon.com>
2020-02-01 12:35:34 -0300

    organize packages

---
+++ src/lambda_api/model.rs
@@ -0,0 +1,34 @@
+use serde_derive::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Deserialize)]
+pub enum HttpMethod {
+    DELETE,
+    GET,
+    HEAD,
+    OPTIONS,
+    PATCH,
+    POST,
+    PUT,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ApiRequest {
+    pub http_method: HttpMethod,
+    pub resource: String,
+    pub headers: Option<HashMap<String, String>>,
+    pub path_parameters: Option<HashMap<String, String>>,
+    pub query_string_parameters: Option<HashMap<String, String>>,
+    pub multi_value_query_string_parameters: Option<HashMap<String, HashMap<String, String>>>,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+#[derive(Default)]
+pub struct ApiResponse {
+    pub status_code: i32,
+    pub body: String,
+    pub is_base64_encoded: bool,
+    pub headers: HashMap<String, String>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,41 @@
+mod lambda_api;
+
+use lambda_api::{ApiRequest, ApiResponse};
 use lambda_runtime::{error::HandlerError, lambda, Context};
-use serde_derive::{Deserialize, Serialize};
+
 use simple_logger;
-use std::collections::HashMap;
 use std::error::Error;
-
-#[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct ApiRequest {
-    query_string_parameters: Option<HashMap<String, String>>,
-}
-
-#[derive(Serialize)]
-#[serde(rename_all = "camelCase")]
-struct ApiResponse {    
-    status_code: i32,
-    body: String,    
-    is_base64_encoded: bool,
-    headers: HashMap<String, String>,
-}
 
 fn main() -> Result<(), Box<dyn Error>> {
     simple_logger::init_with_level(log::Level::Debug)?;
-    lambda!(my_handler);
+    lambda!(api_gateway_handler);
 
     Ok(())
 }
 
-fn my_handler(request: ApiRequest, _c: Context) -> Result<ApiResponse, HandlerError> {
-    Ok(ApiResponse {
-        status_code: 200,
-        body: format!(
-            "{{\"response\": \"Welcome, {}!\" }}",
-            request
-                .query_string_parameters
-                .unwrap_or_default()
-                .get("name")
-                .unwrap_or(&"Stranger".to_string())
-        ),
-        is_base64_encoded: false,
-        headers: HashMap::new(),
-    })
+pub fn api_gateway_handler(request: ApiRequest, _c: Context) -> Result<ApiResponse, HandlerError> {
+    let response = match request.resource.as_str() {
+        "/api/time" => ApiResponse {
+            status_code: 200,
+            ..Default::default()
+        },
+        "/api/time/{user_id}" => ApiResponse {
+            status_code: 200,
+            ..Default::default()
+        },
+        "/api/user_preferences" => ApiResponse {
+            status_code: 200,
+            ..Default::default()
+        },
+        "/api/user_preferences/{user_id}" => ApiResponse {
+            status_code: 200,
+            ..Default::default()
+        },
+        _ => ApiResponse {
+            status_code: 404,
+            ..Default::default()
+        },
+    };
+
+    Ok(response)
 }
